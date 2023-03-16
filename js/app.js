4822c1d5acc1fd9311fdcdf3939cf077b5535bef
AlveeGit <alveekabir1@gmail.com>
2023-03-16 10:30:55 +0600

    second commit

--- js/app.js
+++ js/app.js
@@ -1,29 +1,53 @@
 const searchFood = () => {
-    const searchField = document.getElementById('search-field');
-    const searchText = searchField.value;
-    searchField.value = '';
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+  const searchField = document.getElementById('search-field');
+  const searchText = searchField.value;
+  searchField.value = '';
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
 
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displaySearchResult(data.meals));
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displaySearchResult(data.meals));
 }
 
 const displaySearchResult = meals => {
-    const searchResult = document.getElementById('search-result');
-    meals.forEach(meal => {
-        console.log('meal');
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `<div class="col">
-        <div class="card h-100">
+  const searchResult = document.getElementById('search-result');
+  meals.forEach(meal => {
+    console.log('meal');
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `<div class="col">
+        <div onclick= "loadMealDetail(${meal.idMeal})" class="card h-100">
           <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">${meal.strMeal}</h5>
-            <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+            <p class="card-text">${meal.strInstructions.slice(0, 400)}</p>
           </div>
         </div>
       </div>`;
-        searchResult.appendChild(div);
-    })
+    searchResult.appendChild(div);
+  })
+}
+
+const loadMealDetail = mealId => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displayMealDetail(data.meals[0]));
+}
+
+const displayMealDetail = meal => {
+  console.log(meal);
+  const mealDetails = document.getElementById('meal-details');
+  const div = document.createElement('div');
+  div.classList.add('card');
+  div.innerHTML = `
+  <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${meal.strMeal}</h5>
+    <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+    <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+  </div>
+  `
+  mealDetails.appendChild(div);
 }
