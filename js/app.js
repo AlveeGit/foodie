ee5b9b265045e50ed1ace3b8a72f3e78f672b2d4
AlveeGit <alveekabir1@gmail.com>
2023-03-16 12:24:46 +0600

    final update

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
+// error handling
 document.getElementById('error-message').style.display = 'none';
+
+// search area
 const searchFood = () => {
   const searchField = document.getElementById('search-field');
   const searchText = searchField.value;
@@ -15,25 +18,28 @@ const searchFood = () => {
     .then(data => displaySearchResult(data.meals))
     .catch(error => displayError(error))
 }
+
 //error message
-const displayError = error =>{
+const displayError = error => {
   document.getElementById('error-message').style.display = 'block';
 }
 
 // display all searched result
 const displaySearchResult = meals => {
   const searchResult = document.getElementById('search-result');
-  searchResult.innerHTML= '';
+  searchResult.innerHTML = '';
+
   meals.forEach(meal => {
     console.log('meal');
     const div = document.createElement('div');
     div.classList.add('col');
     div.innerHTML = `<div class="col">
-        <div onclick= "loadMealDetail(${meal.idMeal})" class="card h-100">
+        <div class="card h-100">
           <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">${meal.strMeal}</h5>
-            <p class="card-text">${meal.strInstructions.slice(0, 400)}</p>
+            <p class="card-text">${meal.strInstructions.slice(0, 200)}.....</p>
+            <a onclick="loadMealDetail(${meal.idMeal})" href="#mainBody" class="btn btn-info text-white bg-dark">Details</a>
           </div>
         </div>
       </div>`;
@@ -42,7 +48,6 @@ const displaySearchResult = meals => {
 }
 
 // display selected meal 
-
 const loadMealDetail = mealId => {
   const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
 
@@ -54,14 +59,14 @@ const loadMealDetail = mealId => {
 const displayMealDetail = meal => {
   console.log(meal);
   const mealDetails = document.getElementById('meal-details');
-  mealDetails.innerHTML ='';
+  mealDetails.innerHTML = '';
   const div = document.createElement('div');
   div.classList.add('card');
   div.innerHTML = `
   <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">${meal.strMeal}</h5>
-    <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+    <p class="card-text">${meal.strInstructions}</p>
     <a href="${meal.strYoutube}" class="btn btn-primary">Watch tutorial</a>
   </div>
   `
